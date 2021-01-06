0e7695719a8e190ab9dd5dbb6292fa1808650615
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2021-01-06 12:51:22 +0200

    Update pinned nixpkgs

--- nixos.nix
+++ nixos.nix
@@ -2,7 +2,7 @@ let
   nixpkgs = (import <nixpkgs> { }).fetchFromGitHub {
     owner = "NixOS";
     repo = "nixpkgs";
-    rev = "3320a06049fc259e87a2bd98f4cd42f15f746b96";
-    sha256 = "1g5l186d5xh187vdcpfsz1ff8s749949c1pclvzfkylpar09ldkl";
+    rev = "c59ea8b8a0e7f927e7291c14ea6cd1bd3a16ff38";
+    sha256 = "1ak7jqx94fjhc68xh1lh35kh3w3ndbadprrb762qgvcfb8351x8v";
   };
 in import "${nixpkgs}/nixos"
