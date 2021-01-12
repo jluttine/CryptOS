7cf3b3be4f36a679f66d5cd763ebcfcc6397437d
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2021-01-12 12:52:06 +0200

    Update pinned nixpkgs

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,6 +1,2 @@
-# builtins.fetchGit {
-#   url = "https://github.com/NixOS/nixpkgs.git";
-#   rev = "c59ea8b8a0e7f927e7291c14ea6cd1bd3a16ff38";
-#   #sha256 = "1ak7jqx94fjhc68xh1lh35kh3w3ndbadprrb762qgvcfb8351x8v";
-# }
-builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/c59ea8b8a0e7f927e7291c14ea6cd1bd3a16ff38.tar.gz"
+# nixos-unstable at January 9, 2021
+builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/257cbbcd3ab7bd96f5d24d50adc807de7c82e06d.tar.gz"
